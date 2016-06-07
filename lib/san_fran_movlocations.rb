6ab71adf2e9b923755b58612d6cdff7d51375b7e
ryanameier <rymei10@gmail.com>
2016-06-06 19:46:48 -0700

    added locations class

--- lib/san_fran_movlocations.rb
+++ lib/san_fran_movlocations.rb
@@ -1,5 +1,25 @@
 require "san_fran_movlocations/version"
 
 module SanFranMovlocations
-  # Your code goes here...
+  class Location
+
+    attr_reader :location, :title, :actor_1, :actor_2
+
+    def initialize(hash)
+      @location = hash["locations"]
+      @title = hash["title"]
+      @actor_1 = hash["actor_1"]
+      @actor_2 = hash["actor_2"]
+    end
+
+    def self.all? 
+      locations_array = Unirest.get("http://data.sfgov.org/resource/wwmu-gmzc.json").body
+      location = []
+      locations_array.each do |location_hash|
+            locations << Location.new(location_hash)
+          end
+          return locations
+      end
+    end
+  end
 end
